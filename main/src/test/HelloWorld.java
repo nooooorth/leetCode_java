658860e1ac129a24996eeaaf0dd92c2bb2f93e7f
wjy1478 <liuyizhu@hrbeu.edu.cn>
2020-09-12 14:31:43 +0800

    输出版本信息

--- main/src/test/HelloWorld.java
+++ main/src/test/HelloWorld.java
@@ -3,5 +3,7 @@ package test;
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("hello,world!");
+        String version = System.getProperty("java.version");
+        System.out.println("java version:"+version);
     }
 }
